f4a1761295b13f2c6ed6c136c7a87d7e825180d8
Soichiro Kato <azukibeans.5kilo@gmail.com>
2024-06-13 10:31:27 +0900

    fixed body margin and base background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,15 @@
 </div>
 
 <style>
+  :global(body) {
+    margin: 0;
+  }
+
   div.app-container {
     height: 100vh;
     display: grid;
     grid-template-rows: auto 1fr auto;
+
+    background-color: #f8f8f8;
   }
 </style>
