f4c956585c9352ad93682555cced05833ac2bcec
Soichiro Kato <azukibeans.5kilo@gmail.com>
2024-06-16 01:01:29 +0900

    implemented mood chart page UI

---
+++ src/lib/components/DatePicker.svelte
@@ -0,0 +1,11 @@
+<input type="date" />
+
+<style>
+    input[type="date"] {
+        padding: 4px;
+        border: solid 1px #a8a8a8;
+        border-radius: 5px;
+
+        font-size: 1.1rem;
+    }
+</style>
---
+++ src/routes/chart/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import { Line } from "svelte-chartjs";
+    import {
+        Chart as ChartJS,
+        Title,
+        Tooltip,
+        Legend,
+        LineElement,
+        LinearScale,
+        PointElement,
+        CategoryScale,
+    } from "chart.js";
+    import Button from "$lib/components/Button.svelte";
+    import DatePicker from "$lib/components/DatePicker.svelte";
+
+    const data = {
+        labels: Array.from(new Array(5), (_, i) => i),
+        datasets: [{ label: "label", data: [1, 2, 3, 4, 5] }],
+    };
+
+    ChartJS.register(
+        Title,
+        CategoryScale,
+        LinearScale,
+        Tooltip,
+        Legend,
+        LineElement,
+        PointElement,
+    );
+</script>
+
+<div class="app-container">
+    <h1>Mood Chart</h1>
+    <div class="date-form">
+        <i class="fa-solid fa-calendar-day" />
+        <DatePicker />
+    </div>
+    <div class="mood-chart-container">
+        <Line {data} options={{ responsive: true }} />
+    </div>
+    <Button label="How do you feel now?" />
+</div>
+
+<style>
+    .app-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .date-form {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    .date-form > i {
+        color: #888888;
+        font-size: 0.9rem;
+    }
+</style>
