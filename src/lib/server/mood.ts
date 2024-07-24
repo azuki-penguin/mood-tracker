3495f8dfdefc73f1c053f86fdacfc1f552463236
Soichiro Kato <azukibeans.5kilo@gmail.com>
2024-07-25 08:57:20 +0900

    fixed a method to fetch mood records from mongodb

--- src/lib/server/mood.ts
+++ src/lib/server/mood.ts
@@ -1,13 +1,35 @@
 import { database } from '$lib/server/mongodb';
+import { addDay, dayEnd, dayStart } from '@formkit/tempo';
 
 type MoodData = {
     mood: number;
     note?: string;
 };
 
-export const fetchMoodList = async () => {
-    const mood = await database.collection('mood-list').find();
-    return await mood.map((x) => ({ mood: x.mood, date: x.date })).toArray();
+type MoodRecord = MoodData & {
+    createdAt: Date;
+    id: string;
+};
+
+type SearchMoodInput = {
+    date: Date;
+};
+
+export const fetchMoodList = async (input: SearchMoodInput) => {
+    const from = dayStart(input.date);
+    const to = dayEnd(addDay(from, 7));
+    const mood = await database.collection('moodLogs').find({
+        createdAt: {
+            $gte: from,
+            $lte: to,
+        },
+    });
+    return await mood.map<MoodRecord>((x) => ({
+        mood: x.mood,
+        note: x.note,
+        createdAt: x.createdAt,
+        id: x._id.toString(),
+    })).toArray();
 };
 
 export const addMood = async (moodData: MoodData) =>  {
