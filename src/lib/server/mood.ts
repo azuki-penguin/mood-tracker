4068344993738ae6aa53fdae831c54ab2136071b
Soichiro Kato <azukibeans.5kilo@gmail.com>
2024-07-24 20:06:45 +0900

    implemented a method to save mood record to mongodb

--- src/lib/server/mood.ts
+++ src/lib/server/mood.ts
@@ -1,6 +1,24 @@
 import { database } from '$lib/server/mongodb';
 
+type MoodData = {
+    mood: number;
+    note?: string;
+};
+
 export const fetchMoodList = async () => {
     const mood = await database.collection('mood-list').find();
     return await mood.map((x) => ({ mood: x.mood, date: x.date })).toArray();
+};
+
+export const addMood = async (moodData: MoodData) =>  {
+    const { acknowledged, insertedId } = await database.collection('moodLogs')
+        .insertOne({
+            ...moodData,
+            createdAt: new Date(),
+        });
+    if (acknowledged) {
+        const mood = await database.collection('moodLogs').findOne(insertedId);
+        return await mood;
+    }
+    return null;
 };
