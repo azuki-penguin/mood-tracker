8d4f77cc0b26edda3ff475dc71c635342c2fc516
Soichiro Kato <azukibeans.5kilo@gmail.com>
2024-08-02 19:04:16 +0900

    changed to redirect to login page if unauthenticated

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,3 @@
-import { fetchMoodList } from '$lib/server/mood';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
@@ -6,7 +5,4 @@ export const load: PageServerLoad = async ({ locals }) => {
     if (!locals.currentUser) {
         throw redirect(302, '/login');
     }
-    const now = new Date();
-    const list = await fetchMoodList({ date: now, userId: locals.currentUser.userId });
-    return { list, date: now };
 };
